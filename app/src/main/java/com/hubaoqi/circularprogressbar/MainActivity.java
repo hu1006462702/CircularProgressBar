6a798f3f24b1ba9420e9717bfb6a93a5169e3261
SUNFENG <SUNFENG>
2017-01-06 14:56:43 +0800

    init commit

---
+++ app/src/main/java/com/hubaoqi/circularprogressbar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hubaoqi.circularprogressbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
